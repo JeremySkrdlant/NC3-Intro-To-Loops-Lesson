fb670f5a41db5aebde58cfec6ba178a14f526501
Jeremy Skrdlant <jeremy.skrdlant@nwktc.edu>
2024-12-24 06:52:42 -0700

    Initial Commit

---
+++ LoopsDemo/LoopsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopsDemoApp.swift
+//  LoopsDemo
+//
+//  Created by Jeremy Skrdlant on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct LoopsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoopsDemoTests/LoopsDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoopsDemoTests.swift
+//  LoopsDemoTests
+//
+//  Created by Jeremy Skrdlant on 12/24/24.
+//
+
+import Testing
+@testable import LoopsDemo
+
+struct LoopsDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
